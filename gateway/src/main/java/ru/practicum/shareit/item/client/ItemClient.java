0bcc07c0a114d10279c710cde61ee75870c74e7f
example1 <vali0191@mail.com>
2024-10-03 23:42:05 +0300

    Добавление запросов

--- gateway/src/main/java/ru/practicum/shareit/item/client/ItemClient.java
+++ gateway/src/main/java/ru/practicum/shareit/item/client/ItemClient.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.item.client;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;

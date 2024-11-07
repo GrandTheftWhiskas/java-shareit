e24529dee50e510c0af1af4e3e676adec2807b38
Your Name <you@example.com>
2024-11-07 08:44:44 +0300

    ххх

--- src/main/java/ru/practicum/shareit/ShareItGateway.java
+++ src/main/java/ru/practicum/shareit/ShareItGateway.java
@@ -2,7 +2,7 @@ package ru.practicum.shareit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class ShareItGateway {
 	public static void main(String[] args) {
